f302a7a070d50a34a4c6e4646042f911970fecf7
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-09 18:34:50 +0100

    Add environment variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 import { PagesComponent } from './pages/pages.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { EnvironnmentsComponent } from './environnments/environnments.component';
+import { EnvironmentsComponent } from './environments/environments.component';
+import { UtilsComponent } from './utils/utils.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    EnvironnmentsComponent,
+    EnvironmentsComponent,
+    UtilsComponent
   ],
     imports: [
         BrowserModule,
