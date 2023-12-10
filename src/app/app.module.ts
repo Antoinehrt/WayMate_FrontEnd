8b24534d232b47c548084a85d2fcf970d582ac96
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-10 17:39:23 +0100

    Creation of the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
