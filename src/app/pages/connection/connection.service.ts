a5687f3154d918c1de21aacf527daaf56338fbaf
Antoine <59895814+Antoinehrt@users.noreply.github.com>
2023-12-12 20:48:20 +0100

    Correction of connection.service.ts

--- src/app/pages/connection/connection.service.ts
+++ src/app/pages/connection/connection.service.ts
@@ -8,23 +8,17 @@ import {catchError, map, Observable, of} from "rxjs";
   providedIn: 'root'
 })
 export class ConnectionService {
-  private static _URL_API_AUTH: string = environment.BASE_URL_API + "/authentification"
+  private static _URL_API_AUTH: string = environment.BASE_URL_API + "/authentication/login"
   constructor(private _httpClient: HttpClient) { }
 
-  login(email: string, password: string): boolean {
-    this._httpClient.get<boolean>(ConnectionService._URL_API_AUTH)
+  login(email: string, password: string): Observable<boolean> {
+    return this._httpClient.get<boolean>('${ConnectionService._URL_API_AUTH}?email=${email}&password=${password}')
       .pipe(
-        map(response => {
-          return response;
-        }),
+        map(response => response),
         catchError(error => {
           console.error('An error has occurred during the HTTP request.', error);
           return of(false);
         })
-      )
-      .subscribe(result => {
-        return result;
-      });
-    return false;
+      );
   }
 }
