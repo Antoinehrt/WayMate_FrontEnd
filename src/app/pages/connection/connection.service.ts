af2c9543b4a41add675c17fdad50dc5d24fadb5b
Antoine <59895814+Antoinehrt@users.noreply.github.com>
2023-12-13 12:07:12 +0100

    Correction of login method

--- src/app/pages/connection/connection.service.ts
+++ src/app/pages/connection/connection.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {catchError, map, Observable, of} from "rxjs";
+import {DtoOutputConnection} from "./dto/dto-output-connection";
 
 
 @Injectable({
@@ -11,14 +12,7 @@ export class ConnectionService {
   private static _URL_API_AUTH: string = environment.BASE_URL_API + "/authentication/login"
   constructor(private _httpClient: HttpClient) { }
 
-  login(email: string, password: string): Observable<boolean> {
-    return this._httpClient.get<boolean>('${ConnectionService._URL_API_AUTH}?email=${email}&password=${password}')
-      .pipe(
-        map(response => response),
-        catchError(error => {
-          console.error('An error has occurred during the HTTP request.', error);
-          return of(false);
-        })
-      );
+  login(email: string, password: string): Observable<DtoOutputConnection> {
+    return this._httpClient.get<DtoOutputConnection>(`${ConnectionService._URL_API_AUTH}?email=${email}&password=${password}`);
   }
 }
