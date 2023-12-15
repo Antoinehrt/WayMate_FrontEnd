9126078de4c537b06be74b5ca33a9dfe74d01f14
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-15 17:16:35 +0100

    Addition of home in app-routing and display upon arrival on the site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {RegistrationComponent} from "./pages/registration/registration.component";
 import {ConnectionComponent} from "./pages/connection/connection.component";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
 
 
 const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo:"/home"},
+  {path: "home", component: HomePageComponent},
   {path: "registration", component: RegistrationComponent},
-  {path: "connection", component: ConnectionComponent}
+  {path: "connection", component: ConnectionComponent},
+  {path: "**", redirectTo:"/home"},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
