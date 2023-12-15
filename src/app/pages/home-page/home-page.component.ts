1bc6a2e6c5bc5d4a2f096d63faf7accea83ff0c7
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-15 19:08:02 +0100

    Validators regex to number people and minimum date

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -10,15 +10,19 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class HomePageComponent {
   ImagePath: string;
+  minDate: string;
 
   form: FormGroup = this._fb.group({
     depart: ['', [Validators.required]],
     destination: ['', [Validators.required]],
     date: ['', [Validators.required]],
-    people: ['', [Validators.required]],
+    people: ['', [Validators.required, Validators.pattern("^\\d+$")]],
   });
   constructor(private _fb: FormBuilder, config: NgbCarouselConfig) {
-    this.ImagePath = "assets/img/waymateHome.png"
+    this.ImagePath = "assets/img/waymateHome.png";
+    const currentDate = new Date();
+    this.minDate = currentDate.toISOString().split('T')[0];
+
     config.interval = 5500;
     config.wrap = true;
     config.keyboard = true;
