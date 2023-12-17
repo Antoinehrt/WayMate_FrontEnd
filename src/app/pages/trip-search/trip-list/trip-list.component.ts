32dfd0e1ae47bf614099ad097d346ccc445a6740
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-18 00:52:27 +0100

    Display of trip and address based on a form

--- src/app/pages/trip-search/trip-list/trip-list.component.ts
+++ src/app/pages/trip-search/trip-list/trip-list.component.ts
@@ -3,6 +3,7 @@ import {DtoInputTrip} from "../dtos/dto-input-trip";
 import {TripSearchService} from "../trip-search.service";
 import {DtoInputAddress} from "../dtos/dto-input-address";
 import { forkJoin } from "rxjs";
+import {DataTransferService} from "../../../utils/data-transfer/data-transfer.service";
 @Component({
   selector: 'app-trip-list',
   templateUrl: './trip-list.component.html',
@@ -11,16 +12,20 @@ import { forkJoin } from "rxjs";
 export class TripListComponent implements OnInit {
   trips: DtoInputTrip[] = [];
   addressDepart: DtoInputAddress[] = [];
+  addressDestination: DtoInputAddress[] = [];
+  formData: any = [];
 
-  constructor(private _tripSearch: TripSearchService) {
+  constructor(private _tripSearch: TripSearchService, private sharedDataService: DataTransferService) {
   }
   ngOnInit() {
     this.getAll();
   }
   getAll() {
+    this.sharedDataService.formData$.subscribe(formData => {
+        this.formData = formData;
+    });
     this._tripSearch.getAll().subscribe(trips => {
       this.trips = trips;
-      console.log('Trips:', this.trips);
       this.getAddress();
     });
   }
@@ -30,7 +35,11 @@ export class TripListComponent implements OnInit {
         this.trips.map(trip => this._tripSearch.fetchAddressById(trip.idStartingPoint))
       ).subscribe(addresses => {
         this.addressDepart = addresses;
-        console.log('Addresses:', this.addressDepart);
+      });
+      forkJoin(
+        this.trips.map(trip => this._tripSearch.fetchAddressById(trip.idDestination))
+      ).subscribe(addresses => {
+        this.addressDestination = addresses;
       });
     }
 }
