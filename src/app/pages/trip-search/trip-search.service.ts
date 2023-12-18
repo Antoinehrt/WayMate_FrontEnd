d06231d647fbe9b5ac65dac8ce7b1350e60da9f4
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-18 22:20:26 +0100

    Remove unuse dto

--- src/app/pages/trip-search/trip-search.service.ts
+++ src/app/pages/trip-search/trip-search.service.ts
@@ -4,7 +4,6 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {DtoInputTrip} from "./dtos/dto-input-trip";
 import {DtoInputAddress} from "./dtos/dto-input-address";
-import {DtoOutputAddress} from "./dtos/dto-output-address";
 
 @Injectable({
   providedIn: 'root'
