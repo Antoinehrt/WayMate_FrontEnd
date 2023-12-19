4b729ef16fdbc011e890efbe95607a61ab9e15da
Antoine <59895814+Antoinehrt@users.noreply.github.com>
2023-12-19 16:03:30 +0100

    Creation of the popup to deal with unauthorized action on the site

---
+++ src/app/addon/popup-not-connected/popup-not-connected.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {Route, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-popup-not-connected',
+  templateUrl: './popup-not-connected.component.html',
+  styleUrls: ['./popup-not-connected.component.css']
+})
+export class PopupNotConnectedComponent {
+
+  constructor(private _ref: MatDialogRef<PopupNotConnectedComponent>, private _route: Router) {
+  }
+
+  login(){
+    this._route.navigate(['/connection']);
+    this.closePopup();
+  }
+  register(){
+    this._route.navigate(['/registration']);
+    this.closePopup();
+  }
+
+  closePopup() {
+    this._ref.close();
+  }
+}
