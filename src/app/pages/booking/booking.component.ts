0c15c8b3e76c54f3a3b5ecd274a237e0f9332dae
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-19 14:26:44 +0100

    Get id from url and display it in html

--- src/app/pages/booking/booking.component.ts
+++ src/app/pages/booking/booking.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css']
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit {
+  tripIdS: string | null = "";
+  tripIdI: number = 0;
 
+  constructor(private _route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this._route.paramMap.subscribe(params => {
+      this.tripIdS = params.get('id');
+      if (typeof this.tripIdS === "string") {
+        this.tripIdI = parseInt(this.tripIdS, 10);
+      }
+      console.log('Trip ID:', this.tripIdI);
+    });
+  }
 }
