7bd873173260584b497abb81b133b30a8e4572fe
tchagisvi <118187580+tchagisvi@users.noreply.github.com>
2023-12-20 21:03:42 +0100

    creating profile account

---
+++ src/app/account/profil/profil.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {DtoInputProfil} from "./dtos/dto-input-profil";
+import {DtoInputAddress} from "./dtos/dto-input-address";
+import {DtoOutputUpdateAddress} from "./dtos/dto-output-update-address";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfilService {
+  private environment: any;
+
+  constructor(private _httpClient: HttpClient,
+              /*private _session: SessionService*/) {
+  }
+  fetchProfil(id: number): Observable<DtoInputProfil> {
+    return this._httpClient.get<DtoInputProfil>(this.environment.apiUrlAccount + "/fetch/profil/" + id);
+  }
+
+ /* updateProfil(dto: DtoInputProfil) {
+
+    let dtoUpdate: DtoOutputUpdateDriver = {
+      email: dto.email,
+      firstName: dto.firstName,
+     // idAccount: this._session.getID(),
+      isAdmin: null,
+      phone: dto.phone,
+      lastName: dto.lastName,
+      pictureURL: dto.pictureURL,
+    }
+    return this.updateProfil(dtoUpdate).subscribe();
+  }*/
+
+  /*updateAddress(dto: DtoInputAddress) {
+
+    let dtoAddress:DtoOutputUpdateAddress={
+      idAddress:dto.idAddress,
+      city:dto.city,
+      street:dto.street,
+      number:dto.number,
+      postCode:dto.postCode
+    }
+    return this.updateAddress(dtoAddress).subscribe();
+  }*/
+}
