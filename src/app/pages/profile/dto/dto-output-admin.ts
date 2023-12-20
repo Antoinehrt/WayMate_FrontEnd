77392c85b3fa159828bb8cb60cbd9062a00d100d
Antoine <hauret.antoine@gmail.com>
2023-12-20 17:55:54 +0100

    creation of Dtos that will be used for the profile

---
+++ src/app/pages/profile/dto/dto-output-admin.ts
@@ -0,0 +1,10 @@
+export interface DtoOutputAdmin {
+  id: number;
+  username:string;
+  userType: string;
+  password:string;
+  email: string;
+  birthdate: Date;
+  isBanned: string;
+  phoneNumber: string;
+}
---
+++ src/app/pages/profile/dto/dto-output-passenger.ts
@@ -0,0 +1,14 @@
+export interface DtoOutputPassenger {
+  id: number;
+  username:string;
+  userType: string;
+  password:string;
+  email: string;
+  birthdate: Date;
+  isBanned: string;
+  phoneNumber: string;
+  lastname: string;
+  firstname:string;
+  gender: string;
+  addressId: number
+}
