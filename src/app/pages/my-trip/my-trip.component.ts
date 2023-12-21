c9f6ccfbabb8ad247e1c4dc34ad07d86866b0951
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-22 00:50:33 +0100

    Display all trip of the driver in a table

--- src/app/pages/my-trip/my-trip.component.ts
+++ src/app/pages/my-trip/my-trip.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../../utils/authentication/authentication.service";
 import {MyTripService} from "./my-trip.service";
 import {DtoOutputUser} from "./dtos/dto-output-user";
+import {DtoInputTrip} from "../trip-search/dtos/dto-input-trip";
+import {DtoInputAddress} from "../trip-search/dtos/dto-input-address";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-my-trip',
@@ -10,8 +13,10 @@ import {DtoOutputUser} from "./dtos/dto-output-user";
 })
 export class MyTripComponent implements OnInit {
   user!:DtoOutputUser;
+  groupedTrips: any[] = [];
+  filteredTrips: any[] = [];
 
-  constructor(private _authService: AuthenticationService, private _myTripService: MyTripService) {
+  constructor(private _authService: AuthenticationService, private _myTripService: MyTripService, private _router: Router) {
   }
 
   ngOnInit(): void {
@@ -24,8 +29,40 @@ export class MyTripComponent implements OnInit {
         this._myTripService.getUserFromUsername(value.username).subscribe(
           value => {
             this.user = value;
+            this.getAllTripDetails();
+            console.log(this.user);
           });
       }
     );
+  }
+
+  getAllTripDetails() {
+    this._myTripService.getAllTripDetails().subscribe(data => {
+      this.groupedTrips = this.groupTrips(data.trips, data.addresses);
+      this.groupedTrips.sort((a, b) => new Date(a.trip.date).getTime() - new Date(b.trip.date).getTime());
+      this.filterTrips();
+      console.log(this.filteredTrips);
+    });
+  }
+
+  filterTrips() {
+    this.filteredTrips = this.groupedTrips.filter(trip =>
+      trip.trip.idDriver === this.user.id
+    );
+  }
+
+  private groupTrips(trips: DtoInputTrip[], addresses: DtoInputAddress[]): any[] {
+    return trips.map(trip => {
+      return {
+        trip: trip,
+        departureAddress: addresses.find(addr => addr.id === trip.idStartingPoint),
+        destinationAddress: addresses.find(addr => addr.id === trip.idDestination),
+      };
+    });
+  }
+
+  modifyTrips(id:number){
+    console.log(id);
+    this._router.navigate(['/trip', id]);
   }
 }
