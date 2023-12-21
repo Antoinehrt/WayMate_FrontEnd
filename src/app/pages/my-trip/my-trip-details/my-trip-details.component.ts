5120cb01d388020005d16fa163a16442c1036516
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-22 00:51:17 +0100

    Create trip details component to display selected trip details

---
+++ src/app/pages/my-trip/my-trip-details/my-trip-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-trip-details',
+  templateUrl: './my-trip-details.component.html',
+  styleUrls: ['./my-trip-details.component.css']
+})
+export class MyTripDetailsComponent {
+
+}
