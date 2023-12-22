75bab3e22cc5d9a66355b425cc8fcb129e40f69b
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-22 19:32:08 +0100

    Add verification for display menu item

--- src/app/addon/navbar/navbar.component.ts
+++ src/app/addon/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../../utils/authentication/authentication.service";
 import {Router} from "@angular/router";
 
@@ -7,10 +7,15 @@ import {Router} from "@angular/router";
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   ImagePath: string;
   collapsed = true;
   isConnected = false;
+  passenger = false;
+  driver = false;
+  admin = false;
+  username = "";
+
 
   constructor(private authService: AuthenticationService, private _route:Router) {
     this.ImagePath = "assets/img/WayMate_Logo.png"
@@ -27,6 +32,39 @@ export class NavbarComponent {
     });
   }
 
+  displayPassenger(){
+    this.authService.TestConnectionPassenger().subscribe({
+      next: () => {
+        this.passenger = true;
+      },
+      error: () => {
+        this.passenger = false;
+      }
+    });
+  }
+
+  displayDriver(){
+    this.authService.TestConnectionDriver().subscribe({
+      next: () => {
+        this.driver = true;
+      },
+      error: () => {
+        this.driver = false;
+      }
+    });
+  }
+
+  displayAdmin(){
+    this.authService.TestConnectionDriver().subscribe({
+      next: () => {
+        this.admin = true;
+      },
+      error: () => {
+        this.admin = false;
+      }
+    });
+  }
+
   logout(): void{
     this.authService.logout().subscribe({
       next: value => {
@@ -37,5 +75,20 @@ export class NavbarComponent {
         console.error("Logout failed", err);
       }
     });
+  }
+
+  getUsernameToken() {
+    this.authService.GetUsernameFromToken().subscribe(
+      value => {
+        this.username = value.username;
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.getUsernameToken()
+    this.displayPassenger();
+    this.displayDriver();
+    this.displayAdmin();
   }
 }
